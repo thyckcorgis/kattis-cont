5f2555c183fe0a18404c4ec1856f9a4afca5eb54
Charles Ancheta <55412395+cbebe@users.noreply.github.com>
2023-04-07 15:36:44 -0600

    WIP: Parse submissions

---
+++ oop.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from subprocess import check_output
+# Paste cURL command here surrounded by double quotes
+curl = ""
+
+a = curl.split(' ')
+# response is gzipped so we tell cURL to decompress it first
+cmd = ' '.join(a[:1] + ['--compressed'] + a[1:])
+with open('page.html', 'wb') as f:
+    f.write(check_output(cmd, shell=1))
